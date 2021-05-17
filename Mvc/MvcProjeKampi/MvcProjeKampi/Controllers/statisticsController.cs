27076d56b976e56b9375dbdfd359ff982c934501
furkandvrc <furkandvrc@outlook.com>
2021-05-17 03:44:17 +0300

    Ödev 2

---
+++ Mvc/MvcProjeKampi/MvcProjeKampi/Controllers/statisticsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+
+
+namespace MvcProjeKampi.Controllers
+{
+    public class StatisticController : Controller
+    {
+
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            var totalCategory = _context.Categories.Count();
+            ViewBag.totalNumberOfCategories = totalCategory;
+
+            var softwareCategory = _context.Headings.Count(x => x.CategoryID == 19);
+            ViewBag.softwareCategoryTitleNumber = softwareCategory;
+
+            var writerNameSortByA = _context.Writers.Count(x => x.WriterName.Contains("a")); 
+            ViewBag.writerNameSortByA = writerNameSortByA;
+
+            var mostTitles = _context.Headings.Max(x => x.Category.CategoryName);
+            ViewBag.categoryNameWithTheMostTitles = mostTitles;
+
+            var categoryStatusTrue = _context.Categories.Count(x => x.CategoryStatus == true);
+            ViewBag.activeCategories = categoryStatusTrue;
+
+            return View();
+        }
+    }
+}
